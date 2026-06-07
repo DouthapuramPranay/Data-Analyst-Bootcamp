8ee75d43a8ed443d8f0b3ff4b976383f6f3868ca
DouthapuramPranay <douthapurampranay@gmail.com>
2026-06-07 23:23:57 +0530

    MySQL

--- SQL_project.sql
+++ SQL_project.sql
@@ -21,3 +21,67 @@ select *,
 ROW_NUMBER() over(partition by company,location,industry,total_laid_off,percentage_laid_off,'date',stage,country,funds_raised_millions) as row_num from layoffs_staging
 )select * from duplicate_cte where row_num>1;
 
+with duplicate_cte as(
+select *,
+ROW_NUMBER() over(partition by company,location,industry,total_laid_off,percentage_laid_off,'date',stage,country,funds_raised_millions) as row_num from layoffs_staging
+)delete from duplicate_cte where row_num>1;
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+insert into layoffs_staging2 select *,
+ROW_NUMBER() over(partition by company,location,industry,total_laid_off,percentage_laid_off,'date',stage,country,funds_raised_millions) as row_num from layoffs_staging;
+
+select * from layoffs_staging2;
+
+delete from layoffs_staging2 where row_num>1;
+
+select * from layoffs_staging2;
+
+-- standardization data
+select company,trim(company) from layoffs_staging2 ;
+update layoffs_staging2 set company=trim(company);
+
+select industry from layoffs_staging2;
+
+update layoffs_staging2 set industry='Crypto' where industry like 'Crypto%';
+
+select distinct industry from layoffs_staging2;
+
+select distinct country,trim(trailing '.' from country) from layoffs_staging2;
+
+update layoffs_staging2 set country=trim(trailing '.' from country) where country like 'United States%';
+
+select * from layoffs_staging2;
+
+
+
+select `date`,str_to_date(`date`,'%m/%d/%y') from layoffs_staging2;
+
+update layoffs_staging2 set `date` = str_to_date(`date`,'%m/%d/%Y');
+
+ALTER table layoffs_staging2 modify column `date` DATE;
+
+
+select * from layoffs_staging2 where total_laid_off is null  and percentage_laid_off is null;
+
+select * from layoffs_staging2 where industry is null or industry = '';
+
+select * from layoffs_staging2 where company='airbnb';
+update layoffs_staging2 set industry=NULL where industry='';
+select * from layoffs_staging2 t1 join layoffs_staging2 t2 on t1.company=t2.company where t1.industry is null and t2.industry is not null;
+
+update layoffs_staging2 t1 join layoffs_staging t2 on t1.company=t2.company set t1.industry=t2.industry where t1.industry is null and t2.industry is not null;
+
+select * from layoffs_staging2 where company='airbnb';
