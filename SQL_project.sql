4af22a733d7df563ee9555b6d3e50bfcde99a016
DouthapuramPranay <douthapurampranay@gmail.com>
2026-06-08 20:25:46 +0530

    My_SQL

--- SQL_project.sql
+++ SQL_project.sql
@@ -80,8 +80,20 @@ select * from layoffs_staging2 where industry is null or industry = '';
 
 select * from layoffs_staging2 where company='airbnb';
 update layoffs_staging2 set industry=NULL where industry='';
-select * from layoffs_staging2 t1 join layoffs_staging2 t2 on t1.company=t2.company where t1.industry is null and t2.industry is not null;
+select t1.industry,t2.industry from layoffs_staging2 t1 join layoffs_staging2 t2 on t1.company=t2.company where (t1.industry is null or t1.industry='')
+ and t2.industry is not null;
+ 
 
-update layoffs_staging2 t1 join layoffs_staging t2 on t1.company=t2.company set t1.industry=t2.industry where t1.industry is null and t2.industry is not null;
+update layoffs_staging2 t1 join layoffs_staging2 t2 on t1.company=t2.company set t1.industry=t2.industry where t1.industry is null and t2.industry is not null;
 
-select * from layoffs_staging2 where company='airbnb';
+select * from layoffs_staging2 where company like 'Bally%';
+
+select * from layoffs_staging2;
+select * from layoffs_staging2 where total_laid_off is null and percentage_laid_off is null;
+
+delete from layoffs_staging2 where total_laid_off is null and percentage_laid_off is null;
+
+alter table layoffs_staging2 drop column row_num;
+
+select * from layoffs_staging2;
+
